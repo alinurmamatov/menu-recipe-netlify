d2c1adc3bd2af583779bcc023b2d335756d1ed79
alinurmamatov <alishernurmamatov878@gmail.com>
2022-04-21 02:13:04 +0200

    menu upadets

--- src/context/GeneralContext.jsx
+++ src/context/GeneralContext.jsx
@@ -6,7 +6,10 @@ export const RecipiesContext = createContext([]);
 export const GeneralProvider = ({children}) => {
     const [allRecipes, setAllRecipes] = useState([]);
     const [recipeToSearch, setRecipeToSearch] = useState("chicken");
-
+    const [howManyPages, setHowManyPages] = useState(0);
+    const [pageNow, setPageNow] = useState(0);
+    const howManyElPerPage = 5;
+    
     const CallApi = async() => {
         const req = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${recipeToSearch}&app_id=954b9052&app_key=%20f7a5bbdb740916cd6457971923676d01`);
         setAllRecipes(req.data.hits);
@@ -16,10 +19,15 @@ export const GeneralProvider = ({children}) => {
         CallApi()
     }, [recipeToSearch])
 
+    useEffect(() => {
+        setHowManyPages(allRecipes / howManyElPerPage)
+    }, [allRecipes])
+
     const data = {
         allRecipes: allRecipes,
         recipeToSearch: recipeToSearch,
-        setRecipeToSearch: setRecipeToSearch
+        setRecipeToSearch: setRecipeToSearch,
+        howManyPages: howManyPages
     };
 
     return (
